a5f5eea472879c23acc2d51b54d68dca2d6cfd72
rcavakian <85318873+rcavakian@users.noreply.github.com>
2023-08-18 14:27:56 -0300

    Incluir opção Listar Alunos no switch do menu em main.c

--- Atividades/ProjetoEscola/main.c
+++ Atividades/ProjetoEscola/main.c
@@ -14,6 +14,7 @@ int main() {
 
   printf("\n**\tProjeto Escola\t**\n\n");
   do {
+    printf("\n # MENU PRINCIPAL # \n\n");
     printf("1 - Aluno\n");
     printf("2 - Professor\n");
     printf("3 - Disciplina\n");
@@ -23,40 +24,58 @@ int main() {
 
     switch (opcao) {
     case 1:
-      printf("\n**\tMódulo Aluno\t**\n\n");
+      printf("\n**\tMódulo Aluno\t**\n");
       int opcaoAluno;
       do {
-        printf("1 - Cadastrar Aluno\n");
+        printf("\n1 - Cadastrar Aluno\n");
         printf("2 - Listar Aluno\n");
         printf("3 - Atualizar Aluno\n");
-        printf("4 - Listar Aluno\n");
+        printf("4 - Excluir Aluno\n");
         printf("5 - Voltar MENU anterior\n");
         printf("\nOpção: "); 
         scanf("%d", &opcaoAluno);
 
-      } while (opcaoAluno != 5);
-
-      switch (opcaoAluno) {
+        switch (opcaoAluno) {
       case 1:
-        printf("\n1 - Cadastrar Aluno\n"); 
+        printf("\n1 - Cadastro de Aluno\n");
+        if (contadorAlunos == TAM_alunos) {
+          printf("Lista de alunos CHEIA");
+        }
+        else {
+          printf("Digite a matricula do aluno: ");
+          int matricula;
+          scanf("%d", &matricula);
+          listaAlunos[contadorAlunos] = matricula;
+          printf("\nAluno matrícula %d cadastrado com sucesso!\n", listaAlunos[contadorAlunos]);
+          contadorAlunos++;
+        }
         break;
       case 2:
-        printf("\n2 - Listar Aluno\n");
+        printf("\n2 - Listar Alunos\n\n");
+        if (contadorAlunos == 0) {
+          printf("\n!\tLista de alunos vazia\t!\n");
+        } else {
+          for (int i = 0; i < contadorAlunos; i++) {
+            printf("%d - Matricula: %d\n", i+1, listaAlunos[i]);
+          }
+        }
         break;
       case 3:
-        printf("\n3 - Atualizar Aluno\n");
+        printf("\n3 - Atualização de Aluno\n");
         break;
       case 4:
-        printf("\n4 - Listar Aluno\n");
+        printf("\n4 - Exclusão de Aluno\n");
         break;
       case 5:
         printf("\n");
         break;
-      
       default:
         printf("\n!!\tOpção inválida\t!!\n\n");
         break;
       }
+      } while (opcaoAluno != 5);
+
+      
       break;
     case 2:
       printf("\n**\tMódulo Professor\t**\n\n");
