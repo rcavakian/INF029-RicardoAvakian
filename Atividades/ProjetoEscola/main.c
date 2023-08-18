3172a1d25f0c6456b9124b7149a1cb34daf9e992
rcavakian <85318873+rcavakian@users.noreply.github.com>
2023-08-18 11:11:18 -0300

    Incluir opções no menu principal do Projeto Escola

--- Atividades/ProjetoEscola/main.c
+++ Atividades/ProjetoEscola/main.c
@@ -8,34 +8,67 @@
 
 int main() {
 
+  int listaAlunos[TAM_alunos];
+  int contadorAlunos = 0;
   int opcao;
 
-  printf("\n\t\tProjeto Escola\n");
-  do
-  {
+  printf("\n**\tProjeto Escola\t**\n\n");
+  do {
     printf("1 - Aluno\n");
     printf("2 - Professor\n");
     printf("3 - Disciplina\n");
     printf("4 - Sair\n");
-    printf("Opção: ");
+    printf("\nOpção: ");
     scanf("%d", &opcao);
 
-    switch (opcao)
-    {
+    switch (opcao) {
     case 1:
-      printf("\n\t\tMódulo Aluno\n");
+      printf("\n**\tMódulo Aluno\t**\n\n");
+      int opcaoAluno;
+      do {
+        printf("1 - Cadastrar Aluno\n");
+        printf("2 - Listar Aluno\n");
+        printf("3 - Atualizar Aluno\n");
+        printf("4 - Listar Aluno\n");
+        printf("5 - Voltar MENU anterior\n");
+        printf("\nOpção: "); 
+        scanf("%d", &opcaoAluno);
+
+      } while (opcaoAluno != 5);
+
+      switch (opcaoAluno) {
+      case 1:
+        printf("\n1 - Cadastrar Aluno\n"); 
+        break;
+      case 2:
+        printf("\n2 - Listar Aluno\n");
+        break;
+      case 3:
+        printf("\n3 - Atualizar Aluno\n");
+        break;
+      case 4:
+        printf("\n4 - Listar Aluno\n");
+        break;
+      case 5:
+        printf("\n");
+        break;
+      
+      default:
+        printf("\n!!\tOpção inválida\t!!\n\n");
+        break;
+      }
       break;
     case 2:
-      printf("\n\t\tMódulo Professor\n");
+      printf("\n**\tMódulo Professor\t**\n\n");
       break;
     case 3:
-      printf("\n\t\tMódulo Disciplina\n");
+      printf("\n**\tMódulo Disciplina\t**\n\n");
       break;
     case 4:
-      printf("\n\t\tPrograma finalizado\n");
+      printf("\n**\tPrograma finalizado\t**\n\n");
       break;
     default:
-      printf("\n\t\tOpção inválida!\n");
+      printf("\n!!\tOpção inválida\t!!\n\n");
       break;
     }
 
